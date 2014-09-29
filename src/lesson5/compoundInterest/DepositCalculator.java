ded9899af4231fff61d1cf02bb0fd9a90a4550a9
isem-dev <isem.com@gmail.com>
2014-09-29 17:13:57 +0300

    Compound interest task commit

---
+++ src/lesson5/compoundInterest/DepositCalculator.java
@@ -0,0 +1,18 @@
+package lesson5.compoundInterest;
+
+public class DepositCalculator {
+	
+	public static int darkZone(int monthlyPayment, int interest, int dreamSum) {
+		double monthlyInterest = (double) interest / 12; // interest = 12, monthlyInterest = 1.00
+		double balance = 0;
+		
+		int monthsQty = 0;
+		for(; balance < dreamSum; monthsQty++) {
+			balance = balance + monthlyPayment + (balance + monthlyPayment) * monthlyInterest / 100;
+		}
+		System.out.println("balance = " + (int) balance);
+		System.out.println("months qty = " + monthsQty);
+		
+		return monthsQty / 12;
+	}
+}
---
+++ src/lesson5/compoundInterest/Launcher.java
@@ -0,0 +1,10 @@
+package lesson5.compoundInterest;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("years qty = " + DepositCalculator.darkZone(100, 12, 1000000));
+		
+	}
+
+}
