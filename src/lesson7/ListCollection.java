cdbe3b060ccce48185ba8e44bc816d88b0aa8ecf
isem-dev <isem.com@gmail.com>
2014-10-16 16:57:00 +0300

    lesson7 start

---
+++ src/lesson7/ListCollection.java
@@ -0,0 +1,16 @@
+package lesson7;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ListCollection {
+	
+	List <Integer> numbers = new ArrayList <Integer>();
+	
+	List <String> names = new ArrayList <>(); // <> - diamond operator from Java 7
+	
+	List <List<Integer>>  nembersL = new ArrayList <>();
+	
+	List <Double> numbersD = new LinkedList <>();
+}
